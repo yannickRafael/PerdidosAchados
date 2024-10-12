0b2c21d7daf2922f29f91bcbbbfbee6e1c7bff9f
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 08:49:55 +0200

    renamed and added methods: 1. to find by name 2. to update by id

--- src/main/java/com/sfs/perdidosachados/repository/CategoryRepository.java
+++ src/main/java/com/sfs/perdidosachados/repository/CategoryRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByName(String name);
+    Category updateCategoriesById(int id, Category category);
 
 }
