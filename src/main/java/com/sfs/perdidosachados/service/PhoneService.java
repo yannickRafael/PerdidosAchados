ac40d23e06d9145c28b7865bd039ab1c867ed394
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 21:05:53 +0200

    added service for phone

---
+++ src/main/java/com/sfs/perdidosachados/service/PhoneService.java
@@ -0,0 +1,11 @@
+package com.sfs.perdidosachados.service;
+
+import com.sfs.perdidosachados.repository.PhoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhoneService {
+
+
+}
