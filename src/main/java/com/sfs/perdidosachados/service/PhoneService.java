ba89e0714690ad32948064f64a3b697ceca339af
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 21:08:12 +0200

    added CRUD methods

--- src/main/java/com/sfs/perdidosachados/service/PhoneService.java
+++ src/main/java/com/sfs/perdidosachados/service/PhoneService.java
@@ -1,11 +1,36 @@
 package com.sfs.perdidosachados.service;
 
+import com.sfs.perdidosachados.model.Phone;
 import com.sfs.perdidosachados.repository.PhoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PhoneService {
+    
+    @Autowired
+    private PhoneRepository phoneRepository;
 
+    public List<Phone> findAll() {
+        return phoneRepository.findAll();
+    }
 
+    public Optional<Phone> findById(int id) {
+        return phoneRepository.findById(id);
+    }
+
+    public Phone save(Phone phone) {
+        return phoneRepository.save(phone);
+    }
+
+    public Phone updateById(int id, Phone phone) {
+        return phoneRepository.updateById(id, phone);
+    }
+
+    public void deleteById(int id) {
+        phoneRepository.deleteById(id);
+    }
 }
