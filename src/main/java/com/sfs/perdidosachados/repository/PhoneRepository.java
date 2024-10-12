6a502255c27bfb90dea771256127c758dc51c764
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 21:02:26 +0200

    added method to update specific phone using id

--- src/main/java/com/sfs/perdidosachados/repository/PhoneRepository.java
+++ src/main/java/com/sfs/perdidosachados/repository/PhoneRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PhoneRepository extends JpaRepository<Phone, Integer> {
+
+    Phone updateById(Integer id, Phone phone);
+
 }
