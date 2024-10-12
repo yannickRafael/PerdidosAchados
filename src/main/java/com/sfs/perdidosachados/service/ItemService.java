5e42ba173c698a68cbab9eb7d022b238a25f916c
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 09:00:09 +0200

    added method to fetch all items

--- src/main/java/com/sfs/perdidosachados/service/ItemService.java
+++ src/main/java/com/sfs/perdidosachados/service/ItemService.java
@@ -1,12 +1,19 @@
 package com.sfs.perdidosachados.service;
 
+import com.sfs.perdidosachados.model.Item;
 import com.sfs.perdidosachados.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService {
 
     @Autowired
     private ItemRepository itemRepository;
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
 }
