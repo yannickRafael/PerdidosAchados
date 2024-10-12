a5d30c9f4233798d1f5c97b35c1056034ad03f2e
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 20:59:44 +0200

    added CRUD methods

--- src/main/java/com/sfs/perdidosachados/service/ClaimService.java
+++ src/main/java/com/sfs/perdidosachados/service/ClaimService.java
@@ -1,12 +1,35 @@
 package com.sfs.perdidosachados.service;
 
+import com.sfs.perdidosachados.model.Claim;
 import com.sfs.perdidosachados.repository.ClaimRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClaimService {
 
     @Autowired
     private ClaimRepository claimRepository;
+
+    public List<Claim> getAllClaims() {
+        return claimRepository.findAll();
+    }
+    public Optional<Claim> getClaimById(int id) {
+        return claimRepository.findById(id);
+    }
+
+    public Claim createClaim(Claim claim) {
+        return claimRepository.save(claim);
+    }
+
+    public Claim updateById(int id, Claim claim) {
+        return claimRepository.updateById(id, claim);
+    }
+
+    public void deleteById(int id) {
+        claimRepository.deleteById(id);
+    }
 }
