c74de0f31e49d56f6924c0b43105d8754461cbda
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 21:40:33 +0200

    created controller for all entities to handle requests

---
+++ src/main/java/com/sfs/perdidosachados/controller/CategoryController.java
@@ -0,0 +1,14 @@
+package com.sfs.perdidosachados.controller;
+
+import com.sfs.perdidosachados.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+
+}
