1732f0de852f6620dfe9dcc1e1d4a1455c01e03b
yannickRafael <yannickrafel286@gmail.com>
2024-10-12 22:26:41 +0200

    applied methods from category services

--- src/main/java/com/sfs/perdidosachados/controller/CategoryController.java
+++ src/main/java/com/sfs/perdidosachados/controller/CategoryController.java
@@ -1,58 +1,73 @@
 package com.sfs.perdidosachados.controller;
 
+import com.sfs.perdidosachados.model.Category;
 import com.sfs.perdidosachados.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/getall")
     public ResponseEntity<?> getAll(){
         try {
-
-            return ResponseEntity.ok("");
+            List<Category> categories = categoryService.findAll();
+            return ResponseEntity.ok(categories);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch categories: " + e.getMessage());
         }
     }
 
     @GetMapping("/getById/{id}")
     public ResponseEntity<?> getById(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            Optional<Category> category = categoryService.findById(id);
+            if (category.isPresent()) {
+                return ResponseEntity.ok(category.get());
+            }else {
+                return ResponseEntity.notFound().build();
+            }
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch category: " + e.getMessage());
         }
     }
 
     @GetMapping("/save")
-    public ResponseEntity<?> save(@ResponseBody ){
+    public ResponseEntity<?> save(@RequestBody Category category ){
         try {
-            return ResponseEntity.ok("");
+            Category savedCategory = categoryService.save(category);
+            return ResponseEntity.ok(savedCategory);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save category: " + e.getMessage());
         }
     }
 
     @GetMapping("/update/{id}")
-    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody ){
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Category category ){
         try {
-            return ResponseEntity.ok("");
+            Category updatedCategory = categoryService.updateById(id, category);
+            return ResponseEntity.ok(updatedCategory);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update category: " + e.getMessage());
         }
     }
 
     @GetMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            categoryService.deleteById(id);
+            return ResponseEntity.ok("Deleted");
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch categorias: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete cattegory: " + e.getMessage());
         }
     }
 }
