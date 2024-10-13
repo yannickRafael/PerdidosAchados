991d486753ac03870e99c48ca82fbc238a2d7f78
yannickRafael <yannickrafel286@gmail.com>
2024-10-13 15:10:20 +0200

    added logic on resolv endpoint

--- src/main/java/com/sfs/perdidosachados/controller/ResolvController.java
+++ src/main/java/com/sfs/perdidosachados/controller/ResolvController.java
@@ -1,56 +1,73 @@
 package com.sfs.perdidosachados.controller;
 
+import com.sfs.perdidosachados.model.Resolv;
+import com.sfs.perdidosachados.service.ResolvService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
 public class ResolvController {
 
+    @Autowired
+    private ResolvService resolvService;
+
     @GetMapping("/getall")
     public ResponseEntity<?> getAll(){
         try {
-
-            return ResponseEntity.ok("");
+            List<Resolv> resolvs = resolvService.findAll();
+            return ResponseEntity.status(HttpStatus.FOUND).body(resolvs);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch resolvs: " + e.getMessage());
         }
     }
 
     @GetMapping("/getById/{id}")
     public ResponseEntity<?> getById(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            Optional<Resolv> resolv = resolvService.findById(id);
+            if (resolv.isPresent()) {
+                return ResponseEntity.status(HttpStatus.FOUND).body(resolv.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resolv not found");
+            }
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch resolv: " + e.getMessage());
         }
     }
 
     @GetMapping("/save")
-    public ResponseEntity<?> save(@ResponseBody){
+    public ResponseEntity<?> save(@RequestBody Resolv resolv){
         try {
-            return ResponseEntity.ok("");
+            Resolv savedResolv = resolvService.save(resolv);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedResolv);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create resolv: " + e.getMessage());
         }
     }
 
     @GetMapping("/update/{id}")
-    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody ){
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Resolv resolv){
         try {
-            return ResponseEntity.ok("");
+            Resolv updatedResolv = resolvService.updateById(id, resolv);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedResolv);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update resolv: " + e.getMessage());
         }
     }
 
     @GetMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            resolvService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Resolv deleted");
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch categorias: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete resolv: " + e.getMessage());
         }
     }
 }
