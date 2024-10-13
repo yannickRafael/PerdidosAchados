6c08a5d2244c1b01dbaf943db53a12015e9b6e65
yannickRafael <yannickrafel286@gmail.com>
2024-10-13 13:19:43 +0200

    added logic on local endpoint

--- src/main/java/com/sfs/perdidosachados/controller/LocalController.java
+++ src/main/java/com/sfs/perdidosachados/controller/LocalController.java
@@ -1,56 +1,74 @@
 package com.sfs.perdidosachados.controller;
 
+import com.sfs.perdidosachados.model.Item;
+import com.sfs.perdidosachados.model.Local;
+import com.sfs.perdidosachados.service.LocalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
 public class LocalController {
 
+    @Autowired
+    private LocalService localService;
+
     @GetMapping("/getall")
     public ResponseEntity<?> getAll(){
         try {
-
-            return ResponseEntity.ok("");
+            List<Local> locals = localService.findAll();
+            return ResponseEntity.status(HttpStatus.FOUND).body(locals);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch locals: " + e.getMessage());
         }
     }
 
     @GetMapping("/getById/{id}")
     public ResponseEntity<?> getById(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            Optional<Local> local = localService.findById(id);
+            if (local.isPresent()) {
+                return ResponseEntity.status(HttpStatus.FOUND).body(local.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Local not found");
+            }
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch local: " + e.getMessage());
         }
     }
 
     @GetMapping("/save")
-    public ResponseEntity<?> save(@ResponseBody){
+    public ResponseEntity<?> save(@RequestBody Local local){
         try {
-            return ResponseEntity.ok("");
+            Local savedLocal = localService.save(local);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedLocal);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save local: " + e.getMessage());
         }
     }
 
     @GetMapping("/update/{id}")
-    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody ){
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Local local){
         try {
-            return ResponseEntity.ok("");
+            Local updatedLocal = localService.updateById(id, local);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedLocal);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update local: " + e.getMessage());
         }
     }
 
     @GetMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            localService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Local deleted");
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch categorias: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete local: " + e.getMessage());
         }
     }
 }
