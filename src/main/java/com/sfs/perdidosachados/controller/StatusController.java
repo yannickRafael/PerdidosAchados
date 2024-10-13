98fa2a1cdafbcfa8958ded80b3632222c8c326c8
yannickRafael <yannickrafel286@gmail.com>
2024-10-13 15:14:54 +0200

    added logic on status endpoint

--- src/main/java/com/sfs/perdidosachados/controller/StatusController.java
+++ src/main/java/com/sfs/perdidosachados/controller/StatusController.java
@@ -1,56 +1,73 @@
 package com.sfs.perdidosachados.controller;
 
+import com.sfs.perdidosachados.model.Status;
+import com.sfs.perdidosachados.service.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
 public class StatusController {
 
+    @Autowired
+    private StatusService statusService;
+
     @GetMapping("/getall")
     public ResponseEntity<?> getAll(){
         try {
-
-            return ResponseEntity.ok("");
+            List<Status> statuses = statusService.findAll();
+            return ResponseEntity.status(HttpStatus.FOUND).body(statuses);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch statuses: " + e.getMessage());
         }
     }
 
     @GetMapping("/getById/{id}")
     public ResponseEntity<?> getById(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            Optional<Status> statuses = statusService.findById(id);
+            if (statuses.isPresent()) {
+                return ResponseEntity.status(HttpStatus.FOUND).body(statuses.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Status not found");
+            }
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch status: " + e.getMessage());
         }
     }
 
     @GetMapping("/save")
-    public ResponseEntity<?> save(@ResponseBody){
+    public ResponseEntity<?> save(@RequestBody Status status){
         try {
-            return ResponseEntity.ok("");
+            Status savedStatus = statusService.save(status);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedStatus);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save status: " + e.getMessage());
         }
     }
 
     @GetMapping("/update/{id}")
-    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody ){
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Status status){
         try {
-            return ResponseEntity.ok("");
+            Status updatedStatus = statusService.updateById(id, status);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedStatus);
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch **: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to updated status: " + e.getMessage());
         }
     }
 
     @GetMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id){
         try {
-            return ResponseEntity.ok("");
+            statusService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Status deleted");
         }catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch categorias: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete status: " + e.getMessage());
         }
     }
 }
